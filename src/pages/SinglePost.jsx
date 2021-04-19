b62f310acdd57ab0beb2f0ccde54eba80752a0d6
Ganesh A Mane <ganeshm1503@gmail.com>
2021-04-18 21:43:51 -0700

    Update SinglePost.jsx

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -65,7 +65,7 @@ function SinglePost(props) {
             />
           </Grid.Column>
           <Grid.Column width={10}>
-            <Card fluid>
+            <Card fluid width={600}>
               <Card.Content>
                 <Card.Header>{username}</Card.Header>
                 <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
