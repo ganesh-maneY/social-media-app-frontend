c343671811c8eb46e596661f853dd7ea1d5a833b
Ganesh A Mane <ganeshm1503@gmail.com>
2021-04-18 20:51:48 -0700

    Update SinglePost.jsx

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -64,7 +64,7 @@ function SinglePost(props) {
               float="right"
             />
           </Grid.Column>
-          <Grid.Column width={10}>
+          <Grid.Column width={14}>
             <Card fluid>
               <Card.Content>
                 <Card.Header>{username}</Card.Header>
